b532c6d31e184a22c4a22e692cd191bc9d6f2bc5
Thomas Van <thomas@forixdigital.com>
2019-06-22 10:19:13 +0700

    Create Answers component and implement vue list rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,8 +26,8 @@ Vue.use(Authorization);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component("user-info", require("./components/UserInfo.vue").default);
-Vue.component("answer", require("./components/Answer.vue").default);
 Vue.component("vote", require("./components/Vote.vue").default);
+Vue.component("answers", require("./components/Answers.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -34,10 +34,7 @@
             </div>
         </div>
     </div>
-    @include ('answers._index', [
-        'answers' => $question->answers,
-        'answersCount' => $question->answers_count,
-    ])
+    <answers :answers="{{ $question->answers }}" :count="{{ $question->answers_count }}"></answers>
     @include ('answers._create')
 </div>
 @endsection
