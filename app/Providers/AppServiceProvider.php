76092c246d3ec44b8aac95f16c75678495da6a0c
Thomas Van <minhthong@gmail.com>
2019-06-27 00:12:29 +0700

    Force https in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') == 'production')
-            \URL::forceScheme('https')
+            \URL::forceScheme('https');
     }
 }
